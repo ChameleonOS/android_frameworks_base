f97f5f6995ab64d57424e557d359b750118192ba
Anurag Gupta <anurag.gupta@stericsson.com>
2012-07-18 16:18:35 +0530

    FM Radio: Add support for FM Radio in Android
    Creating interface and framework for using FM Radio
    RX and TX from different vendors.
    
    Signed-off-by: Christian Bejram <christian.bejram@stericsson.com>
    Change-Id: I1a71aed01bfffdddfabf1cdfbfa3707cb1ed016b
    
    Conflicts:
    	core/java/android/app/ContextImpl.java

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -122,6 +122,13 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.stericsson.hardware.fm.IFmReceiver;
+import com.stericsson.hardware.fm.IFmTransmitter;
+import com.stericsson.hardware.fm.FmReceiver;
+import com.stericsson.hardware.fm.FmTransmitter;
+import com.stericsson.hardware.fm.FmReceiverImpl;
+import com.stericsson.hardware.fm.FmTransmitterImpl;
+
 class ReceiverRestrictedContext extends ContextWrapper {
     ReceiverRestrictedContext(Context base) {
         super(base);
@@ -547,6 +554,20 @@ class ContextImpl extends Context {
         registerService(WimaxManagerConstants.WIMAX_SERVICE, new ServiceFetcher() {
                 public Object createService(ContextImpl ctx) {
                     return WimaxHelper.createWimaxService(ctx, ctx.mMainThread.getHandler());
+                }});
+
+        registerService("fm_receiver", new ServiceFetcher() {
+                public Object createService(ContextImpl ctx) {
+                    IBinder b = ServiceManager.getService("fm_receiver");
+                    IFmReceiver service = IFmReceiver.Stub.asInterface(b);
+                    return new FmReceiverImpl(service);
+                }});
+
+        registerService("fm_transmitter", new ServiceFetcher() {
+                public Object createService(ContextImpl ctx) {
+                    IBinder b = ServiceManager.getService("fm_transmitter");
+                    IFmTransmitter service = IFmTransmitter.Stub.asInterface(b);
+                    return new FmTransmitterImpl(service);
                 }});
     }
 
