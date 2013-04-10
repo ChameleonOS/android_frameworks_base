f0dca4cac431cad77fcfb190fb766149de9f5214
Matthias Bosc <matthias.bosc@gmail.com>
2013-04-10 16:20:29 +0200

    Frameworks & SystemUI - additional DC-HSPAP support
    This commit adds some more informations about Dual-Carrier HSPAP
    It allows displaying a "DC" icon on status bar for compatible devices
    and add a "DCHSPAP" value in state phone
    patch 1: add TelephonyManager to fix build
    patch 2: modify ServiceState
    patch 3: switch to cm_strings
    patch 4: fix a mistake
    patch 5: upload a new set of icons, a bit prettier than previous
    patch 6: update icons (more opacity to match colors)
    patch 7: removed drawable-sw*
    
    here is a screenshot for example: http://imgur.com/mQ2du3l,PSyHoqC
    
    Change-Id: I77d05e23c8ad3efdfeb19cf06ed92e7c57105ebe
    
    Conflicts:
    	packages/SystemUI/res/values/cm_strings.xml
    	telephony/java/android/telephony/TelephonyManager.java

--- telephony/java/android/telephony/ServiceState.java
+++ telephony/java/android/telephony/ServiceState.java
@@ -455,7 +455,6 @@ public class ServiceState implements Parcelable {
                 rtString = "LTE";
                 break;
             case RIL_RADIO_TECHNOLOGY_HSPAP:
-            case RIL_RADIO_TECHNOLOGY_DCHSPAP:
                 rtString = "HSPAP";
                 break;
             case RIL_RADIO_TECHNOLOGY_DCHSPAP:
@@ -692,7 +691,6 @@ public class ServiceState implements Parcelable {
         case ServiceState.RIL_RADIO_TECHNOLOGY_LTE:
             return TelephonyManager.NETWORK_TYPE_LTE;
         case ServiceState.RIL_RADIO_TECHNOLOGY_HSPAP:
-        case ServiceState.RIL_RADIO_TECHNOLOGY_DCHSPAP:
             return TelephonyManager.NETWORK_TYPE_HSPAP;
         case ServiceState.RIL_RADIO_TECHNOLOGY_DCHSPAP:
             return TelephonyManager.NETWORK_TYPE_DCHSPAP;
