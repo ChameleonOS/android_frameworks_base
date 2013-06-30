a922c070ec237c58351624e5b1bde8d1b02c0724
0xD34D <clark@scheffsblend.com>
2013-06-30 16:44:48 -0700

    Add a circlular memory indicator to the "clear all" button area in recents panel.
    PS2: Have the CircleMemoryMeter update every 100 milliseconds and be fully self contained so no need to call update from elsewhere.
    
    PS3: Add 2dp padding to the top and right of the meter.
    
    PS4: Clean up formatting.
    
    Change-Id: I315d8bf0f566415f93444ed17891c585d79c9bca

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -715,7 +715,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         if (DEBUG) Log.v(TAG, "Jettison " + ad.getLabel());
         mRecentTaskDescriptions.remove(ad);
         mRecentTasksLoader.remove(ad);
-
         // Handled by widget containers to enable LayoutTransitions properly
         // mListAdapter.notifyDataSetChanged();
 
