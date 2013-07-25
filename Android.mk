557ca4ab2152c997cff5f3400c66519a9dd08f6b
0xD34D <clark@scheffsblend.com>
2013-07-25 16:25:15 -0700

    Fix typo and remove duplicate source file
    Change-Id: I65d6b1f3b9fa4d3e760b4be9b935af365a52cbaa

--- Android.mk
+++ Android.mk
@@ -252,7 +252,6 @@ LOCAL_SRC_FILES += \
 	fmradio/java/com/stericsson/hardware/fm/IOnStereoListener.aidl \
 	fmradio/java/com/stericsson/hardware/fm/IOnExtraCommandListener.aidl \
 	fmradio/java/com/stericsson/hardware/fm/IOnAutomaticSwitchListener.aidl
-	wifi/java/android/net/wifi/p2p/IWifiP2pManager.aidl
 #
 
 
