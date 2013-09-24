537d050250f734ec8800dac62593f32e26321be2
Schubi <schubi@erlangen.ccc.de>
2013-09-24 16:51:51 +0200

    Active display: Catch Resource.NotFoundException
    This caused the SystemUI to FC when the device was powered and a notification was fired.
    Display stayed off and the SystemUI kept restarting until the device was unlocked manually.
    
    Change-Id: Ibfd82242e82aa9ee5c0898f5f0714e490eac0ea9

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -824,6 +824,7 @@ public class ActiveDisplayView extends FrameLayout {
                     }
                 } catch (RemoteException re) {
                 } catch (NameNotFoundException nnfe) {
+                } catch (Resources.NotFoundException e) {
                 }
             }
         });
@@ -937,6 +938,7 @@ public class ActiveDisplayView extends FrameLayout {
                 }
             });
         } catch (NameNotFoundException e) {
+        } catch (Resources.NotFoundException e) {
         }
     }
 
