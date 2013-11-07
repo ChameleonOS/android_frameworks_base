3ab2908c3fd16114d17effd6fa6d8075755bbe06
Rajshekar Eashwarappa <reashw@codeaurora.org>
2013-11-07 12:34:24 +0530

    framework:Thread synchronization for UI update
    Issue : Framework reboot was observed due to out of sync between
            two threads which inturn causes Null pointer exception
            while updating airplanemode.
    Fix : Handled Null pointer exception and the crash is not observed.
    CRs-Fixed: 570122
    
    Change-Id: I2e969f6e7726ffcf8648b366ebb54cb2b0afc292

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -1188,8 +1188,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             if (!mHasTelephony) return;
             final boolean inAirplaneMode = serviceState.getState() == ServiceState.STATE_POWER_OFF;
             mAirplaneState = inAirplaneMode ? ToggleAction.State.On : ToggleAction.State.Off;
-            mAirplaneModeOn.updateState(mAirplaneState);
-            mAdapter.notifyDataSetChanged();
+            mHandler.sendEmptyMessage(MESSAGE_REFRESH_AIRPLANEMODE);
         }
     };
 
@@ -1212,6 +1211,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private static final int MESSAGE_DISMISS = 0;
     private static final int MESSAGE_REFRESH = 1;
     private static final int MESSAGE_SHOW = 2;
+    private static final int MESSAGE_REFRESH_AIRPLANEMODE = 3;
     private static final int DIALOG_DISMISS_DELAY = 300; // ms
 
     private Handler mHandler = new Handler() {
@@ -1228,6 +1228,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 break;
             case MESSAGE_SHOW:
                 handleShow();
+                break;
+            case MESSAGE_REFRESH_AIRPLANEMODE:
+                mAirplaneModeOn.updateState(mAirplaneState);
+                mAdapter.notifyDataSetChanged();
                 break;
             }
         }
